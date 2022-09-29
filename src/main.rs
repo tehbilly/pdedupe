f4651184bf829fb2a3e17810b3627087a0740bc3
William McGann <contact@williammcgann.com>
2022-09-29 12:07:26 -0400

    Update to structopt instead of getopts

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,55 @@
 use std::env;
 use std::path::Path;
-use getopts::Options;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pdedupe")]
+struct Options {
+    /// Environment variable to use.
+    #[structopt(long, default_value = "PATH")]
+    var: String,
+
+    /// When true, print output informational output to stderr.
+    #[structopt(short, long)]
+    verbose: bool,
+
+    /// When true, only allow paths that are resolvable to be included.
+    #[structopt(short, long)]
+    exists: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program_name = args[0].clone();
+    let opts = Options::from_args();
 
-    let mut opts = Options::new();
-    opts.optopt("", "var", "The environment variable to use. Defaults to PATH.", "VARIABLE");
-    opts.optflag("h", "help", "Print help information");
-    opts.optflag("v", "verbose", "Print output to stderr");
-    opts.optflag("e", "exists", "Don't add any paths that aren't resolvable");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
-    };
-
-    let var = match matches.opt_str("var") {
-        Some(v) => v,
-        None => "PATH".to_string()
-    };
-
-    if matches.opt_present("h") {
-        print_usage(&program_name, opts);
-        return;
-    }
-
-    let original = get_env_values(var);
+    let original = get_env_values(opts.var.clone());
     let num_original = original.len();
 
-    if matches.opt_present("v") {
+    if opts.verbose {
         eprintln!("Original number of path entries: {}", num_original);
     }
 
     let mut uniques: Vec<String> = Vec::new();
+
     original.iter().for_each(|p| {
-        if matches.opt_present("e") && !Path::new(&p).exists() {
-            if matches.opt_present("v") {
+        if opts.exists && !Path::new(&p).exists() {
+            if opts.verbose {
                 eprintln!("Not adding non-existing path: {}", p);
             }
             return;
         }
         if !uniques.contains(p) {
             uniques.push(p.to_string());
-        } else if matches.opt_present("v") {
+        } else if opts.verbose {
             eprintln!("Ignoring duplicate entry: {}", p);
         }
     });
 
-    if matches.opt_present("v") {
+    if opts.verbose {
         eprintln!("Removed {} duplicate entries, leaving {} unique entries.", num_original - uniques.len(), uniques.len());
     }
 
     println!("{}", env::join_paths(uniques).unwrap().into_string().unwrap());
-}
-
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("Usage: {} [options]", opts.usage(&brief));
 }
 
 fn get_env_values(var: String) -> Vec<String> {
