4de7496267e45403030e6c97618ac878b61b6ec6
William McGann <contact@williammcgann.com>
2026-05-21 11:42:55 -0400

    Some slight modernization and correctness fixes

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A small command line utility to dedupe/clean the PATH environment variable.
 ### Installation
 
 To install, you must have a rust toolchain installed. Using cargo, run `cargo install --git https://github.com/tehbilly/pdedupe`.
+
+### Usage
+
+- `pdedupe` will simply print out the deduplicated contents of `PATH`
+- `pdedupe --exists` will do the same but also only include paths that actually exist
+- `pdedupe [--exists] ANOTHER_VAR` will do the same but for `ANOTHER_VAR` instead of `PATH`
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,70 @@
-use std::env;
-use std::path::Path;
+use std::error::Error;
+use std::ffi::OsStr;
+use std::io::Write;
+use std::{env, io};
 
-use structopt::StructOpt;
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut exists = false;
+    let mut var_name = None;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "pdedupe")]
-struct Options {
-    /// Environment variable to use.
-    #[structopt(long, default_value = "PATH")]
-    var: String,
-
-    /// When true, print output informational output to stderr.
-    #[structopt(short, long)]
-    verbose: bool,
-
-    /// When true, only allow paths that are resolvable to be included.
-    #[structopt(short, long)]
-    exists: bool,
-}
-
-fn main() {
-    let opts = Options::from_args();
-
-    let original = get_env_values(opts.var.clone());
-    let num_original = original.len();
-
-    if opts.verbose {
-        eprintln!("Original number of path entries: {}", num_original);
-    }
-
-    let mut uniques: Vec<String> = Vec::new();
-
-    original.iter().for_each(|p| {
-        if opts.exists && !Path::new(&p).exists() {
-            if opts.verbose {
-                eprintln!("Not adding non-existing path: {}", p);
+    let mut args = env::args();
+    let bin_name = args.next().unwrap();
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--exists" => exists = true,
+            unknown if unknown.starts_with('-') => {
+                eprintln!("unknown flag: {}", unknown);
+                eprintln!("Usage: {} [--exists] [VAR_NAME]", bin_name);
+                std::process::exit(1);
             }
-            return;
+            var if var_name.is_none() => {
+                var_name = Some(var.to_string());
+            }
+            _ => {
+                eprintln!("Too many arguments provided");
+                eprintln!("Usage: {} [--exists] [VAR_NAME]", bin_name);
+                std::process::exit(1);
+            }
         }
-        if !uniques.contains(p) {
-            uniques.push(p.to_string());
-        } else if opts.verbose {
-            eprintln!("Ignoring duplicate entry: {}", p);
-        }
-    });
-
-    if opts.verbose {
-        eprintln!("Removed {} duplicate entries, leaving {} unique entries.", num_original - uniques.len(), uniques.len());
     }
 
-    println!("{}", env::join_paths(uniques).unwrap().into_string().unwrap());
-}
-
-fn get_env_values(var: String) -> Vec<String> {
-    let value = match env::var(var) {
-        Ok(v) => v,
-        _ => "".to_string()
+    let ev = match var_name {
+        Some(vn) => env::var(&vn),
+        None => env::var("PATH"),
+    };
+    let ev = match ev {
+        Ok(val) => val,
+        Err(err) => {
+            return Err(format!("Error reading from var: {}", err).into());
+        }
     };
 
-    env::split_paths(value.as_str())
-        .map(|p| p.into_os_string().into_string().unwrap())
-        .collect()
+    let uniques = env::split_paths(&ev).fold(Vec::new(), |mut acc, path| {
+        if !acc.contains(&path) {
+            if !exists || path.exists() {
+                acc.push(path);
+            }
+        }
+        acc
+    });
+
+    match env::join_paths(uniques) {
+        Ok(joined) => {
+            print_os_string(&joined);
+            Ok(())
+        }
+        Err(err) => Err(format!("Failed to join paths: {err}").into()),
+    }
+}
+
+#[cfg(unix)]
+fn print_os_string(path: &OsStr) {
+    use std::os::unix::ffi::OsStrExt;
+    let _ = io::stdout().write_all(path.as_bytes());
+    let _ = io::stdout().flush();
+}
+
+#[cfg(not(unix))]
+fn print_os_string(path: &OsStr) {
+    print!("{}", path.to_string_lossy());
 }
